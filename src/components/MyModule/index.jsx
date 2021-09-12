330516cd255e29aa75aa8d539f0e20061eac7eb0
pangqianjin <2594622441@qq.com>
2021-09-12 17:56:22 +0800

    添加更换头像功能

--- src/components/MyModule/index.jsx
+++ src/components/MyModule/index.jsx
@@ -5,6 +5,22 @@ import './index.css'
 
 
 export default class MyModule extends Component {
+    uploadAvatar = async ()=>{
+        const fileHandles = await window.showOpenFilePicker()
+        for(const entry of fileHandles){
+            let url = ''
+            const file = await entry.getFile()
+            if (window.createObjectURL) { // basic
+                url = window.createObjectURL(file) ;
+            }else if (window.webkitURL) { // webkit or chrome
+                url = window.webkitURL.createObjectURL(file) ;
+            }else if (window.URL) { // mozilla(firefox)
+                url = window.URL.createObjectURL(file) ;
+            }
+            document.querySelector('img').setAttribute('src', url)
+        }
+    }
+
     render() {
         const {title, color, rows, avatar} = this.props
         return (
@@ -17,7 +33,7 @@ export default class MyModule extends Component {
                     } 
                     </div>
                     <div className="avatar" style={{display: avatar?'block': 'none'}}>
-                        <span className="tip">修改</span>
+                        <span onClick={this.uploadAvatar} className="tip">修改</span>
                         <img src={require('../../imgs/default-avatar.png').default} alt="点击上传头像" />
                     </div>
                 </div>   
